1efc3e798aff941687fd938c1e09bee62f1388fb
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-16 12:51:51 +0100

    first commit

---
+++ project.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#activate virtual env
+read env_name
+
+source $env_name/scripts/activate
+
+echo "Enter project name:"
+read project_name
+
+#create project
+django-admin startproject $project_name
+
+#enter project dir
+cd $project_name
+
+#initialize git
+git init
+
+#add_repo
+read repo_name
+git remote add origin https://github.com/Abdul12majid/$repo_name.git
+
+
+#create directories to store secret key(s)
+mkdir keys
+cd keys
+touch keys.py
+
+#add secret directories to .gitignore file
+echo 'keys/' > .gitignore
+
+
+echo "Set-up done"
